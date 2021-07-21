acb58caf52e242f4df4d0387e147fa44a937df3d
isabella hochschild <ihochschild@gmail.com>
2021-07-20 17:04:23 -0700

    initial commit

--- Live-Laugh-Love/ViewController.swift
+++ Live-Laugh-Love/ViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    @IBAction func clickMeButtonTapped(_ sender: Any) {
+        
+        let message = "Did you know?"
+        let alertController = UIAlertController(
+            title: "", // This gets overridden below.
+            message: message,
+            preferredStyle: .alert
+        )
+        let OKAction = UIAlertAction(title: "OK", style: .cancel) { _ -> Void in
+        }
+        alertController.addAction(OKAction)
 
-
+        let liveLaughLove = "live, laugh, & love"
+        let lll_font = UIFont(name: "The Skinny", size: 17)!
+        let customTitle:NSString = "I \(liveLaughLove) Swift" as NSString // Use NSString, which lets you call rangeOfString()
+        let attributedString = NSMutableAttributedString(string: customTitle as String)
+        let fontAwesomeAttributes = [
+            NSAttributedString.Key.font: lll_font
+        ]
+        let matchRange = customTitle.range(of: liveLaughLove)
+        attributedString.addAttributes(fontAwesomeAttributes, range: matchRange)
+        alertController.setValue(attributedString, forKey: "attributedTitle")
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
